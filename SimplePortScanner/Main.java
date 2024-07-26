c580b78222c74232bf2111fa4cc301c95688a5e0
Mian Al Ruhanyat <alruhanyat994@gmail.com>
2024-07-27 03:35:26 +0600

    adding in the dev

--- SimplePortScanner/Main.java
+++ SimplePortScanner/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         if(args.length != 3){
-            System.out.println("Usage: java Main <IP Address> <Start Port> <End Port>");
+            System.out.println("Usage: java Main <IP Address> <Start Port> <End Port>\nExample: java Main 127.0.0.1 80 85");
             return;
         }
         String ipAddress = args[0];
